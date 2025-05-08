68b9f4f1df498522d4d4e6474605a8a3bac290a2
Sam <98198530+Samonorom@users.noreply.github.com>
2025-05-08 09:27:18 -0400

    Create disk.go

---
+++ hw7/disk/disk.go
@@ -0,0 +1,54 @@
+package disk
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const blockSize = 512
+
+// WriteBlock writes a block to diskN.dat at the given index.
+func WriteBlock(diskID int, blockIndex int, data []byte) error {
+	if len(data) > blockSize {
+		return fmt.Errorf("block too large: %d > %d", len(data), blockSize)
+	}
+
+	path := filepath.Join("data", fmt.Sprintf("disk%d.dat", diskID))
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	offset := int64(blockIndex * blockSize)
+	_, err = f.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	padded := make([]byte, blockSize)
+	copy(padded, data)
+	_, err = f.Write(padded)
+	return err
+}
+
+// ReadBlock reads a block from diskN.dat at the given index.
+func ReadBlock(diskID int, blockIndex int) ([]byte, error) {
+	path := filepath.Join("data", fmt.Sprintf("disk%d.dat", diskID))
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	offset := int64(blockIndex * blockSize)
+	_, err = f.Seek(offset, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	buf := make([]byte, blockSize)
+	_, err = f.Read(buf)
+	return buf, err
+}
